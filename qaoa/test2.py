3687027009ee1e914a85321bede53202008776da
Sahar Saoudi <151698641+sahar-saoudi@users.noreply.github.com>
2024-11-15 03:34:38 -0500

    qaoa implementation ( uncomplete )

---
+++ qaoa/test2.py
@@ -0,0 +1,50 @@
+from qaoa import Quantum_QAOA
+import numpy as np
+import networkx as nx
+from scipy.spatial import KDTree
+from scipy.spatial.distance import pdist
+
+def random_UD_graph(nqubits, seed):
+    np.random.seed(seed)
+    r_absolute_min = 1
+    blockade_radius = 6.4/5
+    #blockade_radius = 0.8 
+    coords = [np.random.uniform(0, 1, size=(2))]
+    while len(coords) < nqubits:
+        cond = True
+        while cond:
+            #choose a random site to connect to. 
+            ind = np.random.randint(0,len(coords))
+            initial_pos = coords[ind]
+            #propose new site:
+            shift_vector_r = np.random.uniform(r_absolute_min, blockade_radius)
+            angle = 2*np.pi*np.random.rand()
+            new_pos = initial_pos + np.array([shift_vector_r*np.cos(angle), shift_vector_r*np.sin(angle)])
+            #check min
+            R_min = np.min(pdist(coords + [new_pos]))
+            if R_min > r_absolute_min:
+                #accept new coordinate
+                cond = False
+        
+        coords.append(new_pos)
+
+    graph = nx.Graph()
+    edges = KDTree(coords).query_pairs(blockade_radius)
+    graph.add_edges_from(edges)
+
+    return graph
+
+G= nx.Graph()
+edges = np.array([(1, 2), (1, 3), (2,3), (3, 4), (3, 5),(4, 5), (5, 6)])
+G.add_edges_from(edges)
+
+G = random_UD_graph(10, 1654654)
+pos = nx.spring_layout(G, k = 1,   seed = 42)
+
+
+#test =  Quantum_QAOA(np.array(list(pos.values())), 0.6)
+test = Quantum_QAOA(np.array(list(pos.values())), edges=list(G.edges()), layers=3, radius=0.6)
+
+test.print_reg()
+
+test.run(generate_histogram=True)
