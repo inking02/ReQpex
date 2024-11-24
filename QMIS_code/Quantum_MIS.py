55852d9690f4b843ca9f3ab1e71a76c5597001ea
LF-Vigneux <156103879+LF-Vigneux@users.noreply.github.com>
2024-11-23 21:50:36 -0500

    Started the docstrings
    Revert "Started the docstrings"
    
    This reverts commit 75ce295737dac5c61cc375978ff46d74f177d4d6.

--- QMIS_code/Quantum_MIS.py
+++ QMIS_code/Quantum_MIS.py
@@ -1,3 +1,8 @@
+"""
+File containing the class of the quantum analog computing MIS finder method. The class of this classifier and its method are in this class. SOme of the useful 
+fonction of the class are in the QMIS_utils.py file. The function that runs the main algorithm is the .run method.
+"""
+
 import numpy as np
 import networkx as nx
 from pulser import Register, Sequence
@@ -9,6 +14,16 @@ from typing import Callable
 
 class Quantum_MIS:
     def __init__(self, graph: nx.graph) -> None:
+        """
+        Object that can run the quantum analog computing MIS algorithm. To create the object, networkx's graph architecture must be used.
+        A graph with more than 15 atom will not give good results.
+
+        Parameters:
+        - graph (networkx.graph): The graph to find an MIS on.
+
+        Returns:
+        None
+        """
         self.G = graph
         self.pos = nx.spring_layout(
             self.G, seed=42
@@ -18,6 +33,15 @@ class Quantum_MIS:
         self.reg = self.build_reg()
 
     def build_reg(self) -> Register:
+        """
+        Function that creates the pulser resgister for a given graph. It is optimal when the number of atoms is less than eleven.
+
+        Parameters:
+        - None
+
+        Returns:
+        Register: The pulser register of the atoms representating the graph.
+        """
         MAX_D = 35
         MIN_D = 5
         scaled_coords, self.radius = scale_coordinates(
@@ -27,6 +51,15 @@ class Quantum_MIS:
         return reg
 
     def print_reg(self) -> None:
+        """
+        Function that draws the positionnement and radius of the atoms of the quantum architecture.
+
+        Parameters:
+        - None
+
+        Returns:
+        None
+        """
         self.reg.draw(
             blockade_radius=self.radius, draw_graph=True, draw_half_radius=True
         )
@@ -38,17 +71,29 @@ class Quantum_MIS:
         generate_histogram: bool = False,
         file_name: str = "QMIS_histo.pdf",
         progress_bar: bool = True,
-    ):
+    ) -> dict:
+        """
+        Method to run the quantum analog computing MIS algorithm. By using a given pulse, it will find the graph given to the object.
+
+        Parameters:
+        - Pulse (Callable): A callable of a function returning a Pulse class objcet from Pulser's library. It is the pulse given to the set of
+                            the atoms to run the algorithm.
+        - shots (int=1000): The number of times the algotihm must be runned. By default, it is set at 1000.
+        - generate_histogram (bool = False): Generate the result histogram of the runs of the algorithms.
+        - file_name (str = "QMIS_histo.pdf"): The file name that the histogram must be saved as. The filename must also include its path and use the extension png.
+        - progress_bar (bool = True):
+
+        Returns:
+        dict: The counts dictionnary of the results from the shots of the algorithms.
+        """
 
         Omega_r_b = AnalogDevice.rabi_from_blockade(self.radius)
         Omega_pulse_max = AnalogDevice.channels["rydberg_global"].max_amp
         Omega = min(Omega_pulse_max, Omega_r_b)
 
         # creating pulse sequence
-        seq = Sequence(
-            self.reg, AnalogDevice
-        )  # La séquence doit contenir un registre ainsi que la machine choisie
-        seq.declare_channel("ising", "rydberg_global")  # On choisit un pulse global.
+        seq = Sequence(self.reg, AnalogDevice)
+        seq.declare_channel("ising", "rydberg_global")
         seq.add(Pulse(Omega), "ising")
 
         simul = QutipEmulator.from_sequence(seq)
