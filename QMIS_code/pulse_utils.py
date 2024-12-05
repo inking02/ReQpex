4d692f9f9cc03c369b4e8f3795c01fc310cae403
inking02 <ludomartvicotec@gmail.com>
2024-12-05 11:36:27 -0500

    pulse corrected

--- QMIS_code/pulse_utils.py
+++ QMIS_code/pulse_utils.py
@@ -12,26 +12,27 @@ from pulser.waveforms import (
 from pulser import Pulse
 
 
-def Waveform_Pulse(Omega: float, T: float) -> Pulse:
+def Waveform_Pulse(Omega: float, T: float, delta_0 : float = -5, delta_f:float = 5) -> Pulse:
     """
     Creates a waveform pulse object.
 
     Parameters:
     - Omega (float):
     - T (float):
+    - delta_0 (float):
+    - delta_f (float):
 
     Returns:
     Pulse: A pulser pusle object.
     """
     adiabatic_pulse = Pulse(
-        InterpolatedWaveform(T, [1e-9, Omega, 1e-9]),
-        InterpolatedWaveform(T, [-Omega, 0, Omega]),
-        0,
+    InterpolatedWaveform(T, [1e-9, Omega, 1e-9]),
+    InterpolatedWaveform(T, [delta_0, 0, delta_f]),0
     )
     return adiabatic_pulse
 
 
-def Rise_Fall_Waveform(Omega: float, T: float):
+def Rise_Fall_Waveform(Omega: float, T: float, delta_0 : float = -5, delta_f:float = 5):
     """
     Creates a waveform pulse object.
 
@@ -42,19 +43,19 @@ def Rise_Fall_Waveform(Omega: float, T: float):
     Returns:
     Pulse: A pulser pusle object.
     """
-    up = RampWaveform(T / 2, 0, Omega)
-    down = RampWaveform(T / 2, Omega, 0)
-    d_up = RampWaveform(T / 2, -Omega, 0)
-    d_down = RampWaveform(T / 2, 0, Omega)
+    up = RampWaveform(T/2, 0, Omega)
+    down = RampWaveform(T/2, Omega, 0)
+    d_up = RampWaveform(T/2, delta_0, 0)
+    d_down = RampWaveform(T/2, 0, delta_f)
 
     rise_fall_Pulse = Pulse(
-        CompositeWaveform(up, down), CompositeWaveform(d_up, d_down), 0
-    )
-
+        CompositeWaveform(up, down),
+        CompositeWaveform(d_up, d_down),0
+        )
+    
     return rise_fall_Pulse
 
-
-def Blackman_Waveform_Pulse(Omega: float, T: float):
+def Blackman_Waveform_Pulse(Omega: float, T: float, delta_0 : float = -5, delta_f:float = 5):
     """
     Creates a waveform pulse object.
 
@@ -66,7 +67,8 @@ def Blackman_Waveform_Pulse(Omega: float, T: float):
     Pulse: A pulser pusle object.
     """
     Blackman_Pulse = Pulse(
-        BlackmanWaveform(T, Omega), InterpolatedWaveform(T, [-Omega, 0, Omega]), 0
+        BlackmanWaveform(T, Omega),
+        InterpolatedWaveform(T, [delta_0, 0, delta_f]),0
     )
 
     return Blackman_Pulse
@@ -76,7 +78,9 @@ def Constant_pulse_pyramide(
     Omega: float,
     T: float,
     T_pyramide: float,
-    delta: float,
+    delta: float, 
+    delta_0 : float = -5, 
+    delta_f:float = 5
 ):
     """
     Creates a waveform pulse object.
@@ -90,28 +94,38 @@ def Constant_pulse_pyramide(
     Pulse: A pulser pusle object.
     """
 
-    Constant_1 = ConstantWaveform((T - T_pyramide) / 2, Omega - delta)
-    up = RampWaveform(T_pyramide / 2, Omega - delta, Omega)
-    down = RampWaveform(T_pyramide / 2, Omega, Omega - delta)
-    Constant_2 = ConstantWaveform((T - T_pyramide) / 2, Omega - delta)
-
+    Constant_1 = ConstantWaveform((T-T_pyramide)/2, Omega-delta)
+    up = RampWaveform(T_pyramide/2, Omega-delta, Omega)
+    down = RampWaveform(T_pyramide/2, Omega, Omega-delta)
+    Constant_2 = ConstantWaveform((T-T_pyramide)/2, Omega-delta)
+    
     r_Pulse = Pulse(
         CompositeWaveform(Constant_1, up, down, Constant_2),
-        InterpolatedWaveform(T, [-Omega, 0, Omega]),
-        0,
+        InterpolatedWaveform(T, [delta_0, 0, delta_f]),0
     )
     return r_Pulse
 
 
-def rise_sweep_fall(Omega, T):
+def rise_sweep_fall(Omega: float, T: float, delta_0 : float = -5, delta_f:float = 5):
+    """
+    Creates a waveform pulse object.
+
+    Parameters:
+    - Omega (float):
+    - T (float):
+
+
+    Returns:
+    Pulse: A pulser pusle object.
+    """
     rise = RampWaveform(T/4, 0, Omega)
     sweep = ConstantWaveform(T/2, Omega)
     fall = RampWaveform(T/4, Omega, 0)
     Omega_Wave = CompositeWaveform(rise, sweep, fall)
 
-    constant1_d = ConstantWaveform(T/4, -Omega)
-    rise_d = RampWaveform(T/2, -Omega, Omega)
-    constant2_d = ConstantWaveform(T/4, Omega)
+    constant1_d = ConstantWaveform(T/4, delta_0)
+    rise_d = RampWaveform(T/2, delta_0, delta_f)
+    constant2_d = ConstantWaveform(T/4, delta_f)
 
     detuning = CompositeWaveform(constant1_d, rise_d, constant2_d)
     return Pulse(Omega_Wave, detuning, 0)
