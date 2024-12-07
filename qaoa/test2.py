83b411ec1fae0913752e17590f7f62992e66ec29
Sahar Saoudi <151698641+sahar-saoudi@users.noreply.github.com>
2024-12-07 18:14:01 -0500

    qaoa update

--- qaoa/test2.py
+++ qaoa/test2.py
@@ -1,50 +1,24 @@
 from qaoa import Quantum_QAOA
 import numpy as np
 import networkx as nx
-from scipy.spatial import KDTree
-from scipy.spatial.distance import pdist
-from utilss import Pulse_constructor
 import matplotlib.pyplot as plt
 
-def random_UD_graph(nqubits, seed):
-    np.random.seed(seed)
-    r_absolute_min = 1
-    blockade_radius = 6.4/5
-    coords = [np.random.uniform(0, 1, size=(2))]
-    while len(coords) < nqubits:
-        cond = True
-        while cond:
-            #choose a random site to connect to. 
-            ind = np.random.randint(0,len(coords))
-            initial_pos = coords[ind]
-            #propose new site:
-            shift_vector_r = np.random.uniform(r_absolute_min, blockade_radius)
-            angle = 2*np.pi*np.random.rand()
-            new_pos = initial_pos + np.array([shift_vector_r*np.cos(angle), shift_vector_r*np.sin(angle)])
-            #check min
-            R_min = np.min(pdist(coords + [new_pos]))
-            if R_min > r_absolute_min:
-                #accept new coordinate
-                cond = False
-        
-        coords.append(new_pos)
+# Create a graph using NetworkX
+G = nx.Graph()  # Initialize an empty undirected graph
+edges = np.array([(1, 2), (1, 3), (2,3), (3, 4), (3, 5),(4, 5), (5, 6)])  # Define edges as pairs of nodes
+G.add_edges_from(edges)  # Add the defined edges to the graph
 
-    graph = nx.Graph()
-    edges = KDTree(coords).query_pairs(blockade_radius)
-    graph.add_edges_from(edges)
 
-    return graph
+# Visualize the graph
+nx.draw(G, with_labels = True) # Draw the graph with node labels
+plt.show()  # Display the graph visualization
 
-G = nx.Graph()
-edges = np.array([(1, 2), (1, 3), (2,3), (3, 4), (3, 5),(4, 5), (5, 6)])
-G.add_edges_from(edges)
-#G= random_UD_graph(10, 12)
-nx.draw(G, with_labels = True)
-plt.show()
 
-#Pulse = Pulse_constructor(4000, "Rise_Sweep_Fall")
+# Initialize the Quantum_QAOA class with the graph and 2 QAOA layers
+test = Quantum_QAOA(G, layers=2)
 
-test =  Quantum_QAOA(G, layers=2)
+# Print the register to visualize the qubit arrangement and blockade radius
 test.print_reg()
 
+# Run the QAOA algorithm and generate a histogram of results
 test.run(generate_histogram=True)
